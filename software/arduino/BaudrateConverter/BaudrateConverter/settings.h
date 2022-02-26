daf8eabd55430d13de1b73c0571f982308f76185
Tor-Eirik Bakke Lunde <teblunde@gmail.com>
2022-02-26 01:50:23 +0100

    Pitch bend details

--- software/arduino/BaudrateConverter/BaudrateConverter/settings.h
+++ software/arduino/BaudrateConverter/BaudrateConverter/settings.h
@@ -1,8 +1,10 @@
 #ifndef _SETTINGS_H
 #define _SETTINGS_H
 
-#define DEBUG_NOTE
-#define DEBUG_NOTE_CONVERT
+// #define DEBUG_NOTE
+// #define DEBUG_NOTE_DETAILS
+#define DEBUG_PITCH_BEND
+#define DEBUG_PITCH_BEND_DETAILS
 // #define DEBUG_PROGRAM_CHANGE
 // #define DEBUG_CONTROL_CHANGE
 #endif
