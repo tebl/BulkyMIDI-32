c16d9920ce90c943a4487bbeedab8fabcb3fc6d4
Tor-Eirik Bakke Lunde <teblunde@gmail.com>
2022-06-22 00:54:49 +0200

    Read button

--- software/arduino/ChannelMonitor/ChannelMonitor/ChannelMonitor.ino
+++ software/arduino/ChannelMonitor/ChannelMonitor/ChannelMonitor.ino
@@ -1,26 +1,33 @@
 #include <Arduino.h>
 #include <MIDI.h>
+#include <ezButton.h>
 #include "constants.h"
 #include "settings.h"
 #include "channel_monitor.h"
 #include "custom_led.h"
 
 CustomLED activity(PIN_ACTIVITY, ACTIVITY_BRIGHTNESS);
-MIDI_CREATE_DEFAULT_INSTANCE();
 ChannelMonitor channels(PIN_DATA, PIN_CLOCK, PIN_LATCH, PIN_OE);
+ezButton select(PIN_SELECT);
+
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup() {
     activity.boost(2500);
+    select.setDebounceTime(DEBOUNCE_DELAY);
 
     pinMode(PIN_MODE_A, INPUT_PULLUP);
     pinMode(PIN_MODE_B, INPUT_PULLUP);
-
-    pinMode(PIN_SELECT, INPUT_PULLUP);
 
     MIDI.begin(MIDI_CHANNEL_OMNI);
 }
 
 void loop() {
+    select.loop();
+    if (select.isPressed()) {
+        activity.boost(50);
+    }
+
     activity.tick();
     channels.tick();
     if (MIDI.read()) {
--- software/arduino/ChannelMonitor/ChannelMonitor/settings.h
+++ software/arduino/ChannelMonitor/ChannelMonitor/settings.h
@@ -5,10 +5,15 @@
  * activity. Increasing this value from around 100ms to 250ms will make it
  * appear mostly solid.
  */
-#define ACTIVITY_DELAY 250
+#define ACTIVITY_DELAY 50
 #define ACTIVITY_BRIGHTNESS LED_ACT_MEDIUM
 
 /* Defines how long a channel will stay lit on a message. Not as exact, but
  * for people who don't like too much flickering this is a good thing.
  */
 #define CHANNEL_DELAY 250
+
+/* Button debounce delay, meaning that a switch needs to be stable for at
+ * least this many milliseconds before we recognize it as a valid press. 
+ */
+#define DEBOUNCE_DELAY 50
